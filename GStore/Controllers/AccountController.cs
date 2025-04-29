1d54a2dbbfbd069eddd2447de06659793a57f320
kamilisimionato <kamili.simionato@etec.sp.gov.br>
2025-04-28 21:13:20 -0300

    parei pag38

--- GStore/Controllers/AccountController.cs
+++ GStore/Controllers/AccountController.cs
@@ -84,6 +84,13 @@ public async Task<IActionResult> Logout()
     return RedirectToAction("Index", "Home");
 }
 
+[HttpGet]
+public IActionResult Registro()
+{
+    RegistroVM register = new();
+    return ViewModels(register);
+}
+
         public bool IsValidEmail(string email)
         {
             try

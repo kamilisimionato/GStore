3d3b500281ba4d4f9eef5d5d141566237c086a79
kamilisimionato <kamili.simionato@etec.sp.gov.br>
2025-05-23 11:38:07 -0300

    escrever em casa a pág 57

--- GStore/Controllers/AccountController.cs
+++ GStore/Controllers/AccountController.cs
@@ -1,7 +1,7 @@
 using System.Net.Mail;
 using System.Security.Claims;
-using Gstore.Data;
-using Gstore.Helpers;
+using GStore.Data;
+using GStore.Helpers;
 using GStore.Models;
 using GStore.ViewModels;
 using Microsoft.AspNetCore.Identity;
@@ -9,166 +9,157 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace GStore.Controllers;
 
-    public class AccountController : Controller
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+    private readonly SignInManager<Usuario> _signInManager;
+    private readonly UserManager<Usuario> _userManager;
+    private readonly IWebHostEnvironment _host;
+    private readonly AppDbContext _db;
+
+    public AccountController(
+        ILogger<AccountController> logger,
+        SignInManager<Usuario> signInManager,
+        UserManager<Usuario> userManager,
+        IWebHostEnvironment host,
+        AppDbContext db
+    )
     {
-        private readonly ILogger<AccountController> _logger;
-        private readonly SignInManager<Usuario> _signInManager;
-        private readonly UserManager<Usuario> _userManager;
-        private readonly IWebHostEnvironment _host;
-        private readonly AppDbContext _db;
-
-        public AccountController(
-            ILogger<AccountController> logger,
-            SignInManager<Usuario> signInManager,
-            UserManager<Usuario> userManager,
-            IWebHostEnvironment host
-            AppDbContext db
-        )
-        {
-            _logger = logger;
-            _signInManager = signInManager;
-            _userManager = userManager;
-            _host = host;
-            _db = db;
-        }
-
-        [HttpGet]
-        public IActionResult Login(string returnUrl)
-        {
-            LoginVM login = new()
-            {
-                UrlRetorno = returnUrl ?? Url.Content("~/")
-            };
-            return View(login);
-        }
-
-[HttpPost]
-[ValidateAntiForgeryToken]
-public async Task<IActionResult> Login(LoginVM login)
-{
-    if (ModelState.IsValid)
-    {
-        string userName = login.Email;
-        if (IsValidEmail(login.Email))
-        {
-            var user = await _userManager.FindByEmailAsync(login.Email);
-            if (user != null)
-            userName = user.UserName;
-        }
-
-        var result = await _signInManager.PasswordSignInAsync(
-            userName, login.Senha, login.Lembrar, lockoutOnFailure: true
-        );
-
-        if (result.Succeeded) {
-            _logger.LogInformation($"Usuário {login.Email} acessou o sistema");
-            return LocalRedirect(login.UrlRetorno);
-        }
-
-        if (result.IsLockedOut) {
-            _logger.LogWarning($"Usuário {login.Email} está bloqueado");
-            ModelState.AddModelError("", "Sua conta está bloqueada, aguarde alguns minutos e tente novamente!");
-        }
-        else
-        if (result.IsNotAllowed) {
-            _logger.LogWarning($"Usuário {login.Email} não confirmou sua conta");
-            ModelState.AddModelError(string.Empty, "Sua conta não está confirmada, verifique seu email!");
-        }
-        else
-            ModelState.AddModelError(string.Empty, "Usuário e/ou senha inválidos!");
-    }
-    return View(login);
-}
-
-[HttpPost]
-[ValidateAntiForgeryToken]
-public async Task<IActionResult> Logout()
-{
-    _logger.LogInformation($"Usuário {ClaimTypes.Email} fez logoff");
-    await _signInManager.SignOutAsync();
-    return RedirectToAction("Index", "Home");
-}
-
-[HttpGet]
-public IActionResult Registro()
-{
-    RegistroVM register = new();
-    return ViewModels(register);
-}
-
-        public bool IsValidEmail(string email)
-        {
-            try
-            {
-                MailAddress m = new(email);
-                return true;
-            }
-            catch (FormatException)
-            {
-                return false;
-            }
-        }
+        _logger = logger;
+        _signInManager = signInManager;
+        _userManager = userManager;
+        _host = host;
+        _db = db;
     }
 
-// não sei se tá certo //
-[HttpPost]
-[ValidateAntiForgeryToken]
-public async Task<IActionResult> Registro(RegistroVM registro)
-{
-    if (ModelState.IsValid)
+    [HttpGet]
+    public IActionResult Login(string returnUrl)
     {
-        var usuario = Activator.CreateInstance<Usuario>();
-        usuario.Nome = registro.Nome;
-        usuario.DataNascimento = registro.DataNascimento;
-        usuario.UserName = registro.Email;
-        usuario.NormalizedUserName = registro.Email.ToUpper();
-        usuario.Email = registro.Email;
-        usuario.NormalizedEmail = registro.Email.ToUpper();
-        usuario.EmailConfirmed = true;
-        var result = await _userManager.CreateAsync(usuario, registro.Senha);
-
-        if (result.Succeeded)
+        LoginVM login = new()
         {
-            _logger.LogInformation($"Novo usuário registrado com o email {registro.Email}.");
+            UrlRetorno = returnUrl ?? Url.Content("~/")
+        };
+        return View(login);
+    }
 
-            await _userManager.AddToRoleAsync(usuario, "Cliente");
-
-            if (registro.Foto != null)
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginVM login)
+    {
+        if (ModelState.IsValid)
+        {
+            string userName = login.Email;
+            if (IsValidEmail(login.Email))
             {
-                string nomeArquivo = usuario.Id + Path.GetExtension(registro.Foto.FileName);
-                string caminho = Path.Combine(_host.WebRootPath, @"img/usuarios");
-                string novoArquivo = Path.Combine(caminho, nomeArquivo);
-                using (var stream = new FileStream(novoArquivo, FileMode.Create))
+                var user = await _userManager.FindByEmailAsync(login.Email);
+                if (user != null)
+                    userName = user.UserName;
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Senha, login.Lembrar, lockoutOnFailure: true
+            );
+
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário {login.Email} acessou o sistema");
+                return LocalRedirect(login.UrlRetorno);
+            }
+
+            if (result.IsLockedOut)
+            {
+                _logger.LogWarning($"Usuário {login.Email} está bloqueado");
+                ModelState.AddModelError("", "Sua conta está bloqueada, aguarde alguns minutos e tente novamente!");
+            }
+            else
+            if (result.IsNotAllowed)
+            {
+                _logger.LogWarning($"Usuário {login.Email} não confirmou sua conta");
+                ModelState.AddModelError(string.Empty, "Sua conta não está confirmada, verifique seu email!");
+            }
+            else
+                ModelState.AddModelError(string.Empty, "Usuário e/ou senha inválidos!");
+        }
+        return View(login);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout()
+    {
+        _logger.LogInformation($"Usuário {ClaimTypes.Email} fez logoff");
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+
+    [HttpGet]
+    public IActionResult Registro()
+    {
+        RegistroVM register = new();
+        return View(register);
+    }
+
+
+    // não sei se tá certo //
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Registro(RegistroVM registro)
+    {
+        if (ModelState.IsValid)
+        {
+            var usuario = Activator.CreateInstance<Usuario>();
+            usuario.Nome = registro.Nome;
+            usuario.DataNascimento = registro.DataNascimento;
+            usuario.UserName = registro.Email;
+            usuario.NormalizedUserName = registro.Email.ToUpper();
+            usuario.Email = registro.Email;
+            usuario.NormalizedEmail = registro.Email.ToUpper();
+            usuario.EmailConfirmed = true;
+            var result = await _userManager.CreateAsync(usuario, registro.Senha);
+
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Novo usuário registrado com o email {registro.Email}.");
+
+                await _userManager.AddToRoleAsync(usuario, "Cliente");
+
+                if (registro.Foto != null)
                 {
-                    registro.Foto.CopyTo(stream);
+                    string nomeArquivo = usuario.Id + Path.GetExtension(registro.Foto.FileName);
+                    string caminho = Path.Combine(_host.WebRootPath, @"img/usuarios");
+                    string novoArquivo = Path.Combine(caminho, nomeArquivo);
+                    using (var stream = new FileStream(novoArquivo, FileMode.Create))
+                    {
+                        registro.Foto.CopyTo(stream);
+                    }
+                    usuario.Foto = @"\img\usuarios\" + nomeArquivo;
+                    await _db.SaveChangesAsync();
                 }
-                usuario.Foto = @"\img\usuarios\" + nomeArquivo;
-                await _db.SaveChangeAsync();
+                TempData["Success"] = "Conta criada com Sucesso!";
+                return RedirectToAction(nameof(Login));
             }
-            TempData["Success"] = "Conta criada com Sucesso!"
-            return RedirectToAction(nameof(Login));
+
+            foreach (var error in result.Errors)
+                ModelState.AddModelError(string.Empty, TranslateIdentityErrors.TranslateErrorMessage(error.Code));
         }
-
-        foreach (var error in result.Errors)
-            ModelState.AddModelError(string.Empty, TranslateIdentityErrors.TranslateErrorMessage(error.Code));    
+        return View(registro);
     }
-    return View(registro);
-}
 
-public IActionResult AccessDenied()
-{
-    return View();
-}
-
-public bool IsValidEmail(string email)
-{
-    try
+    public IActionResult AccessDenied()
     {
-        MailAddress m = new(email);
-        return true;
+        return View();
     }
-    catch (FormatException)
+
+    public bool IsValidEmail(string email)
     {
-        return false;
+        try
+        {
+            MailAddress m = new(email);
+            return true;
+        }
+        catch (FormatException)
+        {
+            return false;
+        }
     }
 }
